6e89e1c2e6dc1dca9b357c1a993763567d870251
ljf <ljf+git@grimaud.me>
2021-02-19 03:40:23 +0100

    [fix] Dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="virtualenv python3-dev python3-virtualenv python3-pip"
+pkg_dependencies="python3-venv python3-dev python3-pip"
 
 #=================================================
 # PERSONAL HELPERS
