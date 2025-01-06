b1ee9221bf31889ec9cdafef38845c2f01cfcad0
Félix Piédallu <felix@piedallu.me>
2025-01-06 20:22:05 +0100

    Lint conf

--- conf/gunicorn.py
+++ conf/gunicorn.py
@@ -1,11 +1,13 @@
-command = '__INSTALL_DIR__/venv/bin/gunicorn'
-pythonpath = '__INSTALL_DIR__'
+#!/usr/bin/env python3
+
+command = "__INSTALL_DIR__/venv/bin/gunicorn"
+pythonpath = "__INSTALL_DIR__"
 workers = 4
-user = '__APP__'
-bind = 'unix:__INSTALL_DIR__/sock'
-pid = '__INSTALL_DIR__/__APP__-pid'
-errorlog = '/var/log/__APP__/error.log'
-accesslog = '/var/log/__APP__/access.log'
+user = "__APP__"
+bind = "unix:__INSTALL_DIR__/sock"
+pid = "__INSTALL_DIR__/__APP__-pid"
+errorlog = "/var/log/__APP__/error.log"
+accesslog = "/var/log/__APP__/access.log"
 access_log_format = '%({X-Real-IP}i)s %({X-Forwarded-For}i)s %(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-loglevel = 'warning'
+loglevel = "warning"
 capture_output = True
--- conf/warn-about-attacks-on-donation.sh
+++ conf/warn-about-attacks-on-donation.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 grep "POST __PATH__" /var/log/nginx/__DOMAIN__-access.log | grep '" 500 ' || true
